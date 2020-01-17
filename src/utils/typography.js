ca44abe6fa9d3e58978711a814db27ccb1d7e5b1
ActiveCodex <mo.abbas.29@gmail.com>
2020-01-17 19:33:31 +0000

    Move: Font-family to Typogaphy.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,8 +4,8 @@ const typography = new Typography({
     baseFontSize: '18px',
     baseLineHeight: 1.666,
     scaleRatio: 3,
-    headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
-    bodyFontFamily: ['Georgia', 'serif'],
+    headerFontFamily: ['sans-serif', 'Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial'],
+    bodyFontFamily: ['sans-serif', 'Georgia', 'serif'],
   })
 
 export default typography
